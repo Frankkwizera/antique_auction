6500d4df205eeb47adea2a52895fe55f71af27a9
Frank Kwizera <frankpmn@gmail.com>
2021-09-10 04:02:58 +0200

    Added server skeleton tests

---
+++ tests/server/bid_management_test.py
@@ -0,0 +1,20 @@
+__author__ = "Frank Kwizera"
+
+import unittest
+
+
+class BidManagementServerTest(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_register_auto_bid(self):
+        pass
+
+    def test_register_user_auto_bid_configuration(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
---
+++ tests/server/item_management_test.py
@@ -0,0 +1,20 @@
+__author__ = "Frank Kwizera"
+
+import unittest
+
+
+class ItemManagementServerTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_retrieve_all_items(self):
+        pass
+
+    def test_retrieve_item_details(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
